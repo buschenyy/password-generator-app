6526be9dab245ab23c02f04c02aa23babaf51e54
buschenyy <buschenyy@gmail.com>
2023-04-17 19:58:52 +0200

    app skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import './App.css'
 
 function App() {
-
   return (
     <div className="App">
-
+      <h3>Password Generator</h3>
+      <div>
+        <input readOnly value="password" />
+        <span>copy</span>
+      </div>
+      <input type="range" min="1" max="20" value="10" />
+      <br />
+      <label>
+        <input type="checkbox" />
+        Include Uppercase Letters
+      </label>
+      <br />
+      <label>
+        <input type="checkbox" />
+        Include Lowercase Letters
+      </label>
+      <br />
+      <label>
+        <input type="checkbox" />
+        Include Numbers
+      </label>
+      <br />
+      <label>
+        <input type="checkbox" />
+        Include Symbols
+      </label>
+      <br />
+      <div>Strength</div>
+      <button>GENERATE</button>
     </div>
   )
 }
